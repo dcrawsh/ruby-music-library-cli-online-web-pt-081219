7834a92b8b536e928dbdfb188b8f091983c1bf95
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-27 20:03:28 +0000

    Automatically backed up by Learn

--- spec/001_song_basics_spec.rb
+++ spec/001_song_basics_spec.rb
@@ -73,3 +73,55 @@ describe "Song" do
     end
   end
 end
+
+class Song 
+attr_accessor :name, :artist, :genre 
+attr_reader 
+@@all = Array.new 
+
+def self.all 
+@@all  
+end 
+
+def self.destroy_all 
+all.clear
+end 
+
+def self.create(name)
+song = Song.new(name)
+song.save
+song 
+end 
+
+def save 
+Song.all << self 
+end 
+
+def initialize(name, artist = nil, genre = nil)
+@name = name 
+self.artist = artist if artist 
+self.genre = genre if genre 
+end 
+
+def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+end
+
+def genre=(genre)
+  @genre = genre 
+  genre.add_song(self)
+end 
+
+def self.find_by_name(name)
+Song.all.find {|song| song.name == name}
+end 
+
+def self.find_or_create_by_name(name)
+Song.find_by_name(name) if true ? 
+else 
+Song.create(name)
+end 
+
+  
+end 
--- spec/003_genre_basics_spec.rb
+++ spec/003_genre_basics_spec.rb
@@ -73,3 +73,45 @@ describe "Genre" do
     end
   end
 end
+
+class Genre 
+attr_accessor :name 
+@@all = Array.new 
+
+def self.all 
+@@all  
+end 
+
+def self.destroy_all 
+all.clear
+end 
+
+def self.create(name)
+song = Genre.new(name)
+song.save
+song 
+end 
+
+def save 
+Genre.all << self 
+end 
+
+def initialize(name)
+@name = name 
+@songs = Array.new 
+end 
+
+def songs 
+@songs   
+end 
+
+def add_song(song)
+song.genre = self if song.genre == nil
+self.songs << song unless self.songs.include?(song)
+end 
+
+def artists 
+self.songs.map {|songs| songs.artist}.uniq 
+end 
+  
+end 
