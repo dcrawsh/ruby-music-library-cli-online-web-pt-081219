6a1372d3875c85ec92fed5820802f2c5c0875486
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-27 20:45:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,8 +38,7 @@ def genre=(genre)
   genre.add_song(self)
 end 
 
-def self.find_by_name(name)
-Song.all.find {|song| song.name == name}
+##Song.all.find {|song| song.name == name}
 end 
 
 def self.find_or_create_by_name(name)
