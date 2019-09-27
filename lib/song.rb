d4484c5982b2b7fd83bd95296d01692605691517
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-27 20:33:53 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,49 @@
+class Song 
+attr_accessor :name, :artist, :genre 
+attr_reader 
+@@all = Array.new 
+
+def self.all 
+@@all  
+end 
+
+def self.destroy_all 
+all.clear
+end 
+
+def self.create(name)
+song = Song.new(name)
+song.save
+song 
+end 
+
+def save 
+Song.all << self 
+end 
+
+def initialize(name, artist = nil, genre = nil)
+@name = name 
+self.artist = artist if artist 
+self.genre = genre if genre 
+end 
+
+def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+end
+
+def genre=(genre)
+  @genre = genre 
+  genre.add_song(self)
+end 
+
+def self.find_by_name(name)
+Song.all.find {|song| song.name == name}
+end 
+
+def self.find_or_create_by_name(name)
+find_by_name(name) || create(name)
+end 
+
+  
+end 
